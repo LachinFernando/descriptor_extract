b0532ee2e066b2ebdb6ad818927fcfe9ccda7081
LachinFernando <106962694+LachinFernando@users.noreply.github.com>
2023-10-23 07:03:30 +0530

    add(files): apps and dependencies

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+import pandas as pd
+import numpy as np
+
+
+# constants
+IMG_ADDRESS = "https://drzinph.com/wp-content/uploads/2020/05/image-6.png"
+
+
+# functions
+def extract_descriptors(smile: str) -> dict:
+    descriptors = {}
+    processed_smile = Chem.MolFromSmiles(smile)
+
+    for descriptor_name, descriptor_function in Descriptors._descList:
+        try:
+            get_descriptor = descriptor_function(processed_smile)
+            descriptors[descriptor_name] = get_descriptor
+        except Exception as error:
+            print(str(error))
+            descriptors[descriptor_name] = np.nan
+
+    return descriptors
+
+# web app
+
+# title
+st.title("Predictions through Chem Descriptor")
+# set an image
+st.image(IMG_ADDRESS, caption = "Molecular Descriptors")
+
+# text input 
+smile = st.text_input("Enter the Canonical Smile Notation")
+
+if smile:
+    with st.spinner("Extracting Descriptors..."):
+        descriptor_dict = extract_descriptors(smile)
+        #st.dataframe(pd.DataFrame(descriptor_dict, index=[0]))
+        with st.sidebar:
+            # set header
+            st.header("Extracted Descriptors")
+            # set descriptors
+            st.json(descriptor_dict)
+    
+
